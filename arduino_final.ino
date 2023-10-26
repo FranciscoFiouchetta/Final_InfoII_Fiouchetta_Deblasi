95db197dd9b428064d784a2dd9e408ff0f4fdb25
FranciscoFiouchetta <143759062+FranciscoFiouchetta@users.noreply.github.com>
2023-10-25 23:36:16 -0300

    Update arduino_final.ino
    Ordenado y limpieza general del código + Comentarios sobre el funcionamiento de cada parte.

--- arduino_final.ino
+++ arduino_final.ino
@@ -1,30 +1,26 @@
-int pinpuls=2;
+int pin_pulsador=2;
 int pot0=A0;
 int pot1=A1;
 int valor0=0;
 int valor1=0;
 
 void setup() {
-
-//pinMode(pinpuls,INPUT);
+pinMode(pin_pulsador,INPUT);
 Serial.begin(4800);
-
 }
 
 void loop() {
 
-
-valor0=analogRead(pot0) / 11.36;
+valor0 = analogRead(pot0) / 11.36; // Se divide la entrada del primer potenciometro (ángulo) para pasar del rango '0-1023' al rango '0-90'y se almacena ese dato en valor0
 Serial.print(valor0);
 
-valor1=analogRead(pot1) / 10.23;
+valor1 = analogRead(pot1) / 10.23; // Se divide la entrada del segundo potenciometro (velocidad inicial) para pasar del rango '0-1023' al rango '0-100' y se almacena ese dato en valor1
 Serial.print("-");
 Serial.print(valor1);
 
-int estadopuls=digitalRead(pinpuls);
+int estado_pulsador = digitalRead(pin_pulsador); // Se lee la entrada 2 y se guarda su estado en estado_pulsador
 Serial.print("-");
-Serial.println(estadopuls);
-
+Serial.println(estado_pulsador);
 
 delay(100);
 }
