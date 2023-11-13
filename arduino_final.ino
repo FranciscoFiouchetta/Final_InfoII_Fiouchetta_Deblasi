3ff8d8c0c9acb4bbc23e34bddfd772476e0f94b0
agustin-deblasi01 <144489166+agustin-deblasi01@users.noreply.github.com>
2023-11-13 13:36:35 -0300

    Update arduino_final.ino
    Se modifico el parpadeo de la pantalla lcd y se agrego la suma de puntuaciones correcta

--- arduino_final.ino
+++ arduino_final.ino
@@ -14,6 +14,7 @@ String mensaje2="DEBLACHETTA PARABOLIC CANNON";
 int longtexto1=mensaje1.length();
 int longtexto2=mensaje2.length();
 int posicion;
+int puntuacion;
 
 void bienvenida() {
 
@@ -40,10 +41,11 @@ lcd.init();
 lcd.backlight();
 Serial.begin(4800);
 bienvenida();
-//puntuaciones();
-//puntuaciones();
-//puntuaciones();
 lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("ANG:");
+lcd.setCursor(0,1);
+lcd.print("VEL:");
 }
 
 void loop(){
@@ -53,62 +55,59 @@ if (Serial.available() > 0) {
      char letra = Serial.read();
     if (letra == 'R') {
     int puntuacion_rojo = Serial.parseInt();
-            lcd.setCursor(13, 1);
-            //lcd.print("Punt: ");
-            lcd.print(puntuacion_rojo);delay(1000);
-            
-            
+            puntuacion+=100;
+            letra == 'F';     
         }// Mostrar la puntuación recibida desde Processing
    
     else if (letra == 'A') {
     int puntuacion_amarillo = Serial.parseInt();
-            lcd.setCursor(13, 1);
-            //lcd.print("Punt: ");
-            lcd.print(puntuacion_amarillo);
-            delay(1000);
-            
-            
+            puntuacion+=50;
+            letra == 'F';    
         }
      else if (letra == 'B') {
         int puntuacion_blanco = Serial.parseInt();
-           lcd.setCursor(13, 1);
-           //lcd.print("Punt: ");
-           lcd.print(puntuacion_blanco);
-           delay(1000);
-          
-           
+           puntuacion+=25;
+           letra == 'F';    
         }
+           lcd.setCursor(13, 1);
+           lcd.print(puntuacion);
+           delay(1000);
   }
 
 
-valor0=analogRead(pot0) / 11.36;
+int valorpotenciometro0=analogRead(pot0) / 11.36;
+if (valorpotenciometro0 != valor0){
 Serial.print(valor0);
-lcd.setCursor ( 0, 0 );
-lcd.print("ang:");
 lcd.setCursor ( 5, 0 );
-lcd.print(valor0);
-
-valor1=analogRead(pot1) / 8.525;
-Serial.print("-");
+lcd.print("   ");
+lcd.setCursor ( 5, 0 );
+lcd.print(valorpotenciometro0);
+delay(10);
+valor0=valorpotenciometro0;
+}
+int valorpotenciometro1=analogRead(pot1) / 8.525;
+if (valorpotenciometro1!=valor1){
 Serial.print(valor1);
-lcd.setCursor ( 0, 1 );
-lcd.print("vel:");
 lcd.setCursor ( 5, 1 );
-lcd.print(valor1);
-
+lcd.print("   ");
+lcd.setCursor ( 5, 1 );
+lcd.print(valorpotenciometro1);
+delay(10);
+valor1=valorpotenciometro1;
+}
 int estadopuls=digitalRead(pinpuls);
 Serial.print("-");
 Serial.println(estadopuls);
 lcd.setCursor ( 9, 0);
-lcd.print("est:");
+lcd.print("EST:");
 lcd.setCursor (14, 0 );
 lcd.print(estadopuls);
 
-lcd.setCursor(8, 1);
-lcd.print("Punt: ");
+lcd.setCursor(9, 1);
+lcd.print("PNT: ");
  
 
 delay(10);
-lcd.clear();
+
 
 }
